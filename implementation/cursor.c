c88d384a87ce87d55454a0bfa5c78727cc08589c
HctrNvt <hctr.navet@gmail.com>
2026-04-08 23:07:28 +0200

    Ajout cursor et player youpi

---
+++ implementation/cursor.c
@@ -0,0 +1,24 @@
+#include "../header/cursor.h"
+#include <stdlib.h>
+
+
+Cursor * create_Cursor(int i, int j, int max_i, int max_j){
+    Cursor * c = malloc(sizeof(Cursor));
+    c->i = i;
+    c->j = j;
+    c->max_i = max_i;
+    c->max_j = max_j;
+}
+
+Cursor * free_Cursor(Cursor * c){
+    free(c);
+}
+
+void move_Cursor(Cursor * cursor, int dx, int dy){
+    cursor->i += dx;
+    cursor->j += dy;
+}
+
+void show_Cursor(Cursor * c){
+
+}
---
+++ implementation/player.c
@@ -0,0 +1,5 @@
+#include "../header/player.h"
+
+bool can_move(Player * p){
+    return p->move > 0;
+}
